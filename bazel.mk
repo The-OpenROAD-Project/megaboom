f81cc2d2ff00f36336293ab98e5695301d62d011
Øyvind Harboe <oyvind.harboe@zylin.com>
2023-12-22 14:08:56 +0100

    remove cruft
    Signed-off-by: Øyvind Harboe <oyvind.harboe@zylin.com>

--- bazel.mk
+++ bazel.mk
@@ -12,7 +12,6 @@ bazel-synth:
 bazel-floorplan:
 	mkdir -p $(LOG_DIR) $(REPORTS_DIR)
 	touch $(LOG_DIR)/2_3_floorplan_tdms.log
-	$(UNSET_VARS); echo hello
 	$(UNSET_AND_MAKE) do-2_1_floorplan do-2_2_floorplan_io do-2_3_floorplan_tdms do-2_4_floorplan_macro do-2_5_floorplan_tapcell do-2_6_floorplan_pdn do-2_floorplan
 
 # This target does not produce a non-zero exit code if detailed or global
