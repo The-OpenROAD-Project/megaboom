80a1f937ae435727415d8a54c745b15d5abcb60c
Øyvind Harboe <oyvind.harboe@zylin.com>
2024-01-02 08:03:42 +0100

    print target: replace absolute path with ORFS reference
    Signed-off-by: Øyvind Harboe <oyvind.harboe@zylin.com>

--- bazel-print.mk
+++ bazel-print.mk
@@ -6,5 +6,6 @@ bazel-%-print:
 	sed 's/WORK_HOME=\S* //g' | \
 	sed 's/SUB_MAKE = /.\/orfs /g' | \
 	sed 's/\\/\\\\/g' | \
+	awk -v orfs="$$ORFS" '{gsub(orfs, "\\\$$ORFS"); print}' | \
 	sed 's/ --no-print-directory//g' \
 	>$(LOG_DIR)/$*.txt
