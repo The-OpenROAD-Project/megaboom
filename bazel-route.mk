b26778d2849dcedda8af6d20379ada09cb02d684
Øyvind Harboe <oyvind.harboe@zylin.com>
2024-01-03 18:41:42 +0100

    route: fix check-grt
    Signed-off-by: Øyvind Harboe <oyvind.harboe@zylin.com>

--- bazel-route.mk
+++ bazel-route.mk
@@ -1,9 +1,11 @@
-# Same as do-place, support for build systems that require a non-zero exit code
-# and the same artifacts to be produced every time or no artifacts are published
+.PHONY: check-grt
+check-grt:
+	grep -q 1 $(RESULTS_DIR)/grt.ok
+
 .PHONY: bazel-route
 bazel-route:
 	mkdir -p $(RESULTS_DIR) $(LOG_DIR) $(REPORTS_DIR)
 	touch $(REPORTS_DIR)/congestion.rpt
 	touch $(REPORTS_DIR)/5_route_drc.rpt
 	touch $(LOG_DIR)/5_3_route.log
-	$(UNSET_AND_MAKE) do-5_2_fillcell do-5_3_route do-5_route.sdc do-5_route
+	$(UNSET_AND_MAKE) check-grt do-5_2_fillcell do-5_3_route do-5_route.sdc do-5_route
