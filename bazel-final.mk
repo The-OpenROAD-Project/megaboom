b2e37201c2f3d6448a4c71f8c36ac8217e16ab26
Øyvind Harboe <oyvind.harboe@zylin.com>
2024-01-03 18:41:42 +0100

    route: fix check-grt
    Signed-off-by: Øyvind Harboe <oyvind.harboe@zylin.com>

--- bazel-final.mk
+++ bazel-final.mk
@@ -1,10 +1,6 @@
-.PHONY: check-route
-check-route:
-	grep -q 1 $(RESULTS_DIR)/grt.ok
-
 .PHONY: bazel-final
 bazel-final:
 	mkdir -p $(RESULTS_DIR) $(LOG_DIR) $(REPORTS_DIR)
-	$(UNSET_AND_MAKE) check-route do-6_1_fill do-6_1_fill.sdc do-6_final.sdc do-6_report
+	$(UNSET_AND_MAKE) do-6_1_fill do-6_1_fill.sdc do-6_final.sdc do-6_report
 	$(UNSET_AND_MAKE) do-klayout_tech do-klayout do-klayout_wrap do-gds-merged
 	cp $(GDS_MERGED_FILE) $(GDS_FINAL_FILE)
