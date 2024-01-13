500b78f9b93ba537d35274c658f8613cdcd7beb9
Øyvind Harboe <oyvind.harboe@zylin.com>
2024-01-13 08:14:55 +0100

    Revert ".bazelrc: do not rely on parsing username"
    Unfortunately, it didn't work...
    
    bazelisk shutdown
    bazelisk build ChipTop_grt
    WARNING: Remote Cache: Failed to get credentials for 'https://storage.googleapis.com:443/megaboom-bazel-artifacts' from helper '/home/oyvind/megaboom/cred_helper.py oyvind@openroad.tools': Cannot run program "/home/oyvind/megaboom/cred_helper.py oyvind@openroad.tools" (in directory "/home/oyvind/megaboom"): error=2, No such file or directory
    
    This reverts commit b8e8f55ea66db00e71effccc6b0f8d859426c6f4.

--- cred_helper.py
+++ cred_helper.py
@@ -6,21 +6,24 @@ import re
 import sys
 
 
-def get_gcloud_auth_token(user):
+def get_gcloud_auth_token():
     with open(".bazelrc") as f:
         all = f.read()
+    # The username is in the .bazelrc file as "# user: <username>"
+    # fish it out using a regex
+    USER = re.search(r"# user: (.*)", all).group(1)
 
     # Run gcloud command to get the authentication token
     result = subprocess.run(
-        ["gcloud", "auth", "print-access-token", user],
+        ["gcloud", "auth", "print-access-token", USER],
         capture_output=True, text=True, check=True)
     token = result.stdout.strip()
     return token
 
 
-def generate_credentials(user):
+def generate_credentials():
     # Get the Bearer token from gcloud
-    bearer_token = get_gcloud_auth_token(user)
+    bearer_token = get_gcloud_auth_token()
 
     # Create the JSON object with the required format
     credentials = {
@@ -32,10 +35,10 @@ def generate_credentials(user):
 
 
 def main():
-    if len(sys.argv) != 2:
-        sys.exit("Usage: python credential_helper.py <USER_EMAIL>")
+    if len(sys.argv) != 2 or sys.argv[1] != "get":
+        sys.exit("Usage: python credential_helper.py get")
 
-    credentials = generate_credentials(sys.argv[1])
+    credentials = generate_credentials()
     print(json.dumps(credentials, indent=2))
 
 
