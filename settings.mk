f723fbcc6481ad6d17a73818d5b58cb5ce9063e0
Øyvind Harboe <oyvind.harboe@zylin.com>
2024-01-17 14:12:18 +0100

    settings.mk: elapsed-all is now in ORFS
    Signed-off-by: Øyvind Harboe <oyvind.harboe@zylin.com>

--- settings.mk
+++ settings.mk
@@ -5,8 +5,4 @@ export DESIGN_CONFIG?=config.mk
 export DESIGN_NAME?=ChipTop
 export WORK_HOME?=bazel-bin
 
-.PHONY: elapsed-all
-elapsed-all:
-	@$(UTILS_DIR)/genElapsedTime.py -d $(shell find bazel-bin/logs/asap7/*/base -type d)
-
 include bazel-memory.mk
