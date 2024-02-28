e3fe23654d7c18c00ab9cd38b10c9c77ee124707
Pawel Czarnecki <pczarnecki@antmicro.com>
2024-02-28 13:23:04 +0000

    Remove settings.mk
    Signed-off-by: Pawel Czarnecki <pczarnecki@antmicro.com>

--- settings.mk
+++
@@ -1,8 +0,0 @@
-# Not used by Bazel, only for convenience when
-# using ./orfs make for local inspection and testing
-
-export DESIGN_CONFIG?=config.mk
-export DESIGN_NAME?=ChipTop
-export WORK_HOME?=bazel-bin
-
-include $(BAZEL_ORFS)/memory-bazel.mk
