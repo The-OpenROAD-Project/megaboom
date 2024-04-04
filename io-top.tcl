a86d70a5405c09592498f0b01b6aaac5c852460f
Pawel Czarnecki <pczarnecki@antmicro.com>
2024-04-04 12:28:28 +0200

    Update constraint handling
    Signed-off-by: Pawel Czarnecki <pczarnecki@antmicro.com>

--- io-top.tcl
+++ io-top.tcl
@@ -1,3 +1,5 @@
-source util.tcl
+set script_path [ file dirname $::env(IO_CONSTRAINTS) ]
+# We expect util.tcl in the same directory as this file
+source $script_path/util.tcl
 
 set_io_pin_constraint -region top:* -pin_names [match_pins (io|auto)_.*]
