30e07a143b280a71d1c0cefdf4d2beaa10a81559
Øyvind Harboe <oyvind.harboe@zylin.com>
2024-05-11 09:54:17 +0200

    orfs: fewer surprises
    Signed-off-by: Øyvind Harboe <oyvind.harboe@zylin.com>

--- io-top.tcl
+++ io-top.tcl
@@ -1,5 +1,3 @@
-set script_path [ file dirname $::env(IO_CONSTRAINTS) ]
-# We expect util.tcl in the same directory as this file
-source $script_path/util.tcl
+source util.tcl
 
 set_io_pin_constraint -region top:* -pin_names [match_pins (io|auto)_.*]
