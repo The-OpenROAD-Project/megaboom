54794c1fd4e5776fe8820856189eb775dc5225cf
Illia Vysochyn <ivysochyn@antmicro.com>
2024-07-31 16:04:04 +0200

    CI: Update workflow
    Signed-off-by: Illia Vysochyn <ivysochyn@antmicro.com>

--- .github/scripts/build_local_target.sh
+++ .github/scripts/build_local_target.sh
@@ -3,25 +3,32 @@
 set -e
 
 target_name=${TARGET:-"tag_array_64x184"}
-flow=${FLOW:-"local_make"}
 if [[ -z "$STAGES" ]]; then
-  STAGES=("synth_sdc" "synth" "floorplan" "place" "cts" "generate_abstract")
+  STAGES=("synth" "floorplan" "place" "cts")
 else
   eval "STAGES=($STAGES)"
 fi
 
-echo "Build tag_array_64x184 macro"
-for stage in ${STAGES[@]}
+echo "Build ${target_name} macro"
+for stage in "${STAGES[@]}"
 do
   if [[ -z $SKIP_BUILD ]] ; then
-    echo "query make script target"
-    bazel query ${target_name}_${stage}_scripts
-    bazel query ${target_name}_${stage}_scripts --output=build
-    echo "build make script"
-    bazel build --subcommands --verbose_failures --sandbox_debug ${target_name}_${stage}_scripts
+    echo "query dependency target"
+    bazel query "${target_name}_${stage}_deps"
+    bazel query "${target_name}_${stage}_deps" --output=build
+    echo "build dependency"
+    bazel run --subcommands --verbose_failures --sandbox_debug "${target_name}_${stage}_deps" -- "$(pwd)/build"
   fi
   if [[ -z $SKIP_RUN ]] ; then
     echo "run make script"
-    ./bazel-bin/${target_name}_${stage}_${flow} "bazel-"${stage}
+    build/make "${stage}"
   fi
 done
+
+if [[ -z $SKIP_BUILD && -z $SKIP_ABSTRACT ]]; then
+    echo "query abstract target"
+    bazel query "${target_name}_generate_abstract"
+    bazel query "${target_name}_generate_abstract" --output=build
+    echo "build abstract"
+    bazel build --subcommands --verbose_failures --sandbox_debug "${target_name}_generate_abstract"
+fi
