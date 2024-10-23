15809fbdb14d65532885b58d3d9f0716785a3a45
Øyvind Harboe <oyvind.harboe@zylin.com>
2024-10-23 11:57:48 +0200

    reports: output report after floorplan, place, cts
    Signed-off-by: Øyvind Harboe <oyvind.harboe@zylin.com>

--- etc/BuildMegaboom.sh
+++ etc/BuildMegaboom.sh
@@ -8,5 +8,7 @@ exec 2>&1
 # Check GCP service account entitlements first
 test/cred_helper_test.py
 
-bazel build wns_report BoomTile_grt --keep_going
-cat bazel-bin/BoomTile_wns_report.md
+bazel build wns_report_floorplan wns_report_place wns_report_cts BoomTile_grt --keep_going
+cat bazel-bin/BoomTile_wns_floorplan_report.md
+cat bazel-bin/BoomTile_wns_place_report.md
+cat bazel-bin/BoomTile_wns_cts_report.md
--- wns_report.py
+++ wns_report.py
@@ -69,11 +69,12 @@ def print_log_dir_times(f):
 
 
 def main():
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <sweep_file>")
+    if len(sys.argv) != 3:
+        print("Usage: python script.py <sweep_file> stage")
         sys.exit(1)
 
     sweep_file = sys.argv[1]
+    stage = sys.argv[2]
     with open(sweep_file, "r") as file:
         sweep_json = json.load(file)
     sweep = sweep_json["sweep"]
@@ -87,7 +88,7 @@ def main():
     table_data = None
     for variant in sweep:
         slack_file = os.path.join(
-            os.path.dirname(sweep_file), "BoomTile_" + variant + ".yaml"
+            os.path.dirname(sweep_file), "BoomTile_" + variant + "_" + stage + ".yaml"
         )
         with open(slack_file, "r") as file:
             stats = yaml.safe_load(file)
