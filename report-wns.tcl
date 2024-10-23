aee64f0f5a4f70999d5923090ecc530aedb442fc
Øyvind Harboe <oyvind.harboe@zylin.com>
2024-10-23 20:33:30 +0200

    report: use open.tcl which reads in timing properly
    Signed-off-by: Øyvind Harboe <oyvind.harboe@zylin.com>

--- report-wns.tcl
+++ report-wns.tcl
@@ -1,28 +1,7 @@
 # Test this on some simple design in ORFS:
 #
-# REPORT_STAGE=place OUTFILE=blah.txt make run RUN_SCRIPT=~/megaboom/report-wns.tcl
-source $::env(SCRIPTS_DIR)/load.tcl
-switch -exact -- $::env(REPORT_STAGE) {
-  "floorplan" {
-    load_design 2_floorplan.odb 2_floorplan.sdc
-  }
-  "place" {
-    load_design 3_place.odb 3_place.sdc
-  }
-  "cts" {
-    load_design 4_cts.odb 4_cts.sdc
-  }
-  "grt" {
-    load_design 5_1_grt.odb 4_cts.sdc
-  }
-  "route" {
-    load_design 5_2_route.odb 4_cts.sdc
-  }
-  default {
-    puts "Unknown design: $::env(REPORT_STAGE)"
-    exit 1
-  }
-}
+# ODB_FILE=results/asap7/base/2_floorplan.odb OUTFILE=blah.txt make run RUN_SCRIPT=~/megaboom/report-wns.tcl
+source $::env(SCRIPTS_DIR)/open.tcl
 
 set paths [find_timing_paths -path_group reg2reg -sort_by_slack -group_count 1]
 set path [lindex $paths 0]
