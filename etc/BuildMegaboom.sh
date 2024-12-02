448d3cd838cc6025b636e2d6e4d740269689ad63
Øyvind Harboe <oyvind.harboe@zylin.com>
2024-12-02 13:58:38 +0100

    sweep: use bazel-orfs sweep
    Signed-off-by: Øyvind Harboe <oyvind.harboe@zylin.com>

--- etc/BuildMegaboom.sh
+++ etc/BuildMegaboom.sh
@@ -8,10 +8,10 @@ exec 2>&1
 # Check GCP service account entitlements first
 test/cred_helper_test.py
 
-bazel build sweep_parallel --keep_going
+bazel build BoomTile_sweep_parallel --keep_going
 # If we're not sweeping grt, then we could have
 # started wns_report sooner, but at least we're not
 # running multiple grt builds in parallel, which makes
 # the server fall over.
-bazel build --jobs 1 wns_report BoomTile_grt --keep_going
+bazel build --jobs 1 BoomTile_wns_report BoomTile_grt --keep_going
 cat bazel-bin/BoomTile_wns_report.md
