af46929641c0240cc6fc8c472475d6cdec28226f
Øyvind Harboe <oyvind.harboe@zylin.com>
2025-03-06 01:07:29 +0100

    synth: parallel
    "bazel run update" to update snapshots
    
    snapshots are information about the design stored in
    git, such as macro placement or list of modules
    to be flattened.
    
    Signed-off-by: Øyvind Harboe <oyvind.harboe@zylin.com>

--- write_macros.tcl
+++ write_macros.tcl
@@ -8,11 +8,32 @@ load_design 2_floorplan.odb 2_floorplan.sdc
 write_macro_placement $filename.tmp
 set f [open $filename.tmp r]
 set content [read $f]
-puts "content: $content"
 set content [string map {"/" "."} $content]
-puts "content after: $content"
 close $f
 
 set f [open $filename w]
 puts -nonewline $f $content
+close $f
+
+set db [ord::get_db]
+set chip [$db getChip]
+set block [$chip getBlock]
+set modules [$block getModules]
+# get set of names of masters of modules
+set masters [dict create]
+foreach module $modules {
+    #set master [$module getMaster]
+    dict set masters [$module getName] 1
+}
+# get list of names sorted to make it predictable
+set masters [dict keys $masters]
+set masters [lsort $masters]
+
+set f [open [file join $::env(WORK_HOME) "kept.bzl"] "w"]
+puts $f "# buildifier: disable=module-docstring"
+puts $f "KEPT_MODULES = \["
+foreach master $masters {
+    puts $f "    \"$master\","
+}
+puts $f "\]"
 close $f
