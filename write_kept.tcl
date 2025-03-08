f76c140487f3e2da87c419e916d4bce11d8142b3
Øyvind Harboe <oyvind.harboe@zylin.com>
2025-03-08 17:52:21 +0100

    write_kept: updated
    Signed-off-by: Øyvind Harboe <oyvind.harboe@zylin.com>

---
+++ write_kept.tcl
@@ -0,0 +1,24 @@
+yosys -import
+
+# stdcells are in .v file
+read_verilog $::env(RESULTS_DIR)/1_synth.v
+
+tee -q -o keep.txt -s kept ls A:keep*=1
+
+set f [open keep.txt]
+set lines [split [read $f] \n]
+close $f
+
+set lines [lrange $lines 2 end-1]
+set lines [lmap line $lines {string trim $line}]
+set lines [lsort $lines]
+
+set f [open [file join $::env(WORK_HOME) "kept.bzl"] "w"]
+puts $f "# buildifier: disable=module-docstring"
+puts $f "# asdfsafds"
+puts $f "KEPT_MODULES = \["
+foreach master $lines {
+    puts $f "    \"$master\","
+}
+puts $f "\]"
+close $f
