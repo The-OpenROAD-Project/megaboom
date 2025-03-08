8cd4da318610164a9e7b2a05ab37c0ceda9e8ea9
Øyvind Harboe <oyvind.harboe@zylin.com>
2025-03-08 17:52:21 +0100

    write_kept: updated
    Signed-off-by: Øyvind Harboe <oyvind.harboe@zylin.com>

--- write_macros.tcl
+++ write_macros.tcl
@@ -14,26 +14,3 @@ close $f
 set f [open $filename w]
 puts -nonewline $f $content
 close $f
-
-set db [ord::get_db]
-set chip [$db getChip]
-set block [$chip getBlock]
-set modules [$block getModules]
-# get set of names of masters of modules
-set masters [dict create]
-foreach module $modules {
-    #set master [$module getMaster]
-    dict set masters [$module getName] 1
-}
-# get list of names sorted to make it predictable
-set masters [dict keys $masters]
-set masters [lsort $masters]
-
-set f [open [file join $::env(WORK_HOME) "kept.bzl"] "w"]
-puts $f "# buildifier: disable=module-docstring"
-puts $f "KEPT_MODULES = \["
-foreach master $masters {
-    puts $f "    \"$master\","
-}
-puts $f "\]"
-close $f
